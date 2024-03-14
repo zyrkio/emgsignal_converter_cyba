e55d6debf5109d536205ce4ed38812966d53f722
zyrkio <61494336+zyrkio@users.noreply.github.com>
2024-03-14 17:26:09 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ namespace EMGConversion {
     export function convertFilterRectifyAndCalculateRMS(signal: number): number {
         const operatingVoltage = 3.3;
         const sensorGain = 1009;
-        const alpha = 0.1; // Filterkonstante für den Tiefpassfilter
+        const alpha = 25; // Filterkonstante für den Tiefpassfilter
 
         // Konvertierung in Millivolt
         signal = (signal / 1024 - 0.5) * operatingVoltage / sensorGain * 1000;
